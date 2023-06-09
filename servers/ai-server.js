64caa217be3737b1be11af3bc1f821f43927e334
Avaer Kazmer <a@modules.io>
2023-06-09 13:57:56 -0400

    Update server code

--- servers/ai-server.js
+++ servers/ai-server.js
@@ -1,98 +1,63 @@
-// import dotenv from 'dotenv';
-// import {Configuration, OpenAIApi} from 'openai';
-// import {getFormData} from '../utils/http-utils.js';
-// import {blob2img} from '../utils/convert-utils.js';
 import {
   Readable,
 } from 'stream';
 
-// const configuration = new Configuration({
-//   apiKey: OPENAI_API_KEY,
-//   // formDataCtor: FormData,
-// });
-// const openai = new OpenAIApi(configuration);
-
-//
-
-const createImageBlob = async (prompt, {
-  n = 1,
-  size = '1024x1024',
-  apiKey,
-}) => {
-  // const response = await openai.createImage({
-  //   prompt,
-  //   n: 1,
-  //   size: "1024x1024",
-  // });
-  const res = await fetch(`https://api.openai.com/v1/images/generations`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${apiKey}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      prompt,
-      n,
-      size,
-    }),
-  });
-  const responseData = await res.json();
-  if (!res.ok) {
-    console.log('got error response', res.ok, res.status, responseData);
-  }
-  const image_url = responseData.data[0].url;
-
-  const res2 = await fetch(image_url);
-  const blob = await res2.blob();
-  return blob;
-};
-
-const editRequestBlob = async (req, {
-  apiKey,
-}) => {
-  // console.log('req 1', new Error().stack);
-  const contentType = req.headers['content-type'];
-  // console.log('req 2');
-  const response = await fetch(`https://api.openai.com/v1/images/edits`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${apiKey}`,
-      'Content-Type': contentType,
-    },
-    // pipe the node stream to the fetch body
-    body: req,
-    duplex: 'half',
-  });
-  if (response.ok) {
-    const responseData = await response.json();
-    let image_url = responseData.data[0].url;
-
-    const res = await fetch(image_url);
-    const blob = await res.blob();
-    return blob;
-  } else {
-    const text = await response.text();
-    console.warn('got error response', text, response.headers);
-    throw new Error(text);
-  }
-};
-
 export class AiServer {
   constructor({
-    apiKey,
+    openAiApiKey,
     elevenLabsApiKey,
     anthropicApiKey,
+    // replicateApiToken,
+    blockadelabsApiKey,
   }) {
-    if (!apiKey) {
-      console.warn('ai server got no api key');
+    if (!openAiApiKey || !elevenLabsApiKey || !anthropicApiKey /*|| !replicateApiToken*/ || !blockadelabsApiKey) {
+      console.warn('ai server missing api keys', {
+        openAiApiKey: !!openAiApiKey,
+        elevenLabsApiKey: !!elevenLabsApiKey,
+        anthropicApiKey: !!anthropicApiKey,
+        // replicateApiToken: !!replicateApiToken,
+        blockadelabsApiKey: !!blockadelabsApiKey,
+      });
       debugger;
+      throw new Error('missing api keys: ' + JSON.stringify({
+        openAiApiKey: !!openAiApiKey,
+        elevenLabsApiKey: !!elevenLabsApiKey,
+        anthropicApiKey: !!anthropicApiKey,
+        // replicateApiToken: !!replicateApiToken,
+        blockadelabsApiKey: !!blockadelabsApiKey,
+      }, null, 2));
     }
 
-    this.apiKey = apiKey;
+    this.openAiApiKey = openAiApiKey;
     this.elevenLabsApiKey = elevenLabsApiKey;
     this.anthropicApiKey = anthropicApiKey;
+    // this.replicateApiToken = replicateApiToken;
+    this.blockadelabsApiKey = blockadelabsApiKey;
   }
   async handleRequest(req, res) {
+    const _proxy = async (req, res, u) => {
+      console.log('proxy', {u});
+
+      const headers = {};
+      for (const k in req.headers) {
+        if (k !== 'host') {
+          headers[k] = req.headers[k];
+        }
+      }
+      const opts = {
+        method: req.method,
+        headers,
+        duplex: 'half',
+      };
+      if (['POST', 'PUT', 'DELETE'].includes(req.method)) {
+        opts.body = req;
+      }
+      
+      const proxyRes = await fetch(u, opts);
+      res.status(proxyRes.status);
+      Readable.fromWeb(proxyRes.body).pipe(res);
+    };
+
     try {
       // console.log('ai server handle request yyy', req.url);
 
@@ -111,7 +76,7 @@ export class AiServer {
         const proxyRes = await fetch(`https://api.openai.com/v1/${sub}`, {
           method: 'POST',
           headers: {
-            'Authorization': `Bearer ${this.apiKey}`,
+            'Authorization': `Bearer ${this.openAiApiKey}`,
             'Content-Type': 'application/json',
           },
           // pipe the node request to the fetch body
@@ -155,7 +120,7 @@ export class AiServer {
           method: req.method,
           headers: {
             // ...req.headers,
-            'Authorization': `Bearer ${this.apiKey}`,
+            'Authorization': `Bearer ${this.openAiApiKey}`,
             'Content-Type': req.headers['content-type'],
             'Content-Length': req.headers['content-length'],
             'User-Agent': req.headers['user-agent'],
@@ -228,44 +193,93 @@ export class AiServer {
           console.warn('got api error response', text);
           res.status(500).send(text);
         }
-      } else if (match = req.url.match(/^\/api\/image-ai\/([^\/\?]+)/)) {
-        throw new Error('should not be used');
+      // } else if (['/api/imageSegmentation/get_boxes'].some(prefix => req.url.startsWith(prefix))) {
+      //   const form = new multiparty.Form();
+      //   const data = await new Promise((resolve, reject) => {
+      //     const fields = {};
+      //     const files = {};
 
-        const method = match[1];
+      //     form.on('part', function(part) {
+      //       if (part.filename) {
+      //         // 'part' is a file
+      //         let buffers = [];
+      //         part.on('data', function(chunk) {
+      //           buffers.push(chunk);
+      //         });
 
-        // console.log('match 2', match);
+      //         part.on('end', function() {
+      //           const buffer = Buffer.concat(buffers);
+      //           buffer.name = part.filename;
+      //           buffer.type = mime.lookup(part.filename);
+      //           files[part.name] = buffer;
+      //         });
+              
+      //         part.on('error', function(err) {
+      //           reject(err);
+      //         });
+              
+      //       } else {
+      //         // 'part' is a field
+      //         let value = '';
+      //         part.on('data', function(chunk) {
+      //           value += chunk;
+      //         });
 
-        switch (method) {
-          case 'createImageBlob': {
-            // read query string
-            const {prompt, n, size} = req.query;
-            const blob = await createImageBlob(prompt, {
-              n,
-              size,
-              apiKey: this.apiKey,
-            });
-            const arrayBuffer = await blob.arrayBuffer();
-            const buffer = Buffer.from(arrayBuffer);
-            res.setHeader('Content-Type', blob.type);
-            res.end(buffer);
-            break;
-          }
-          case 'editImgBlob': {
-            const blob = await editRequestBlob(req, {
-              apiKey: this.apiKey,
-            });
-            const arrayBuffer = await blob.arrayBuffer();
-            const buffer = Buffer.from(arrayBuffer);
-            res.setHeader('Content-Type', blob.type);
-            res.end(buffer);
-            break;
-          }
-          default: {
-            console.warn('method not found', method);
-            res.send(404);
-            break;
-          }
-        }
+      //         part.on('end', function() {
+      //           fields[part.name] = value;
+      //         });
+              
+      //         part.on('error', function(err) {
+      //           reject(err);
+      //         });
+      //       }
+      //     });
+
+      //     form.on('error', (err) => {
+      //       reject(err);
+      //     });
+
+      //     form.on('close', () => {
+      //       resolve({ fields, files });
+      //     });
+
+      //     form.parse(req);
+      //   });
+
+      //   console.log('pipe 1');
+      //   const b64 = data.files.img_file.toString('base64');
+      //   const ext = data.files.img_file.type;
+      //   console.log('pipe 2', {ext});
+      //   const prefix = 'data:' + ext + ';base64,';
+      //   const dataUri = `${prefix}${b64}`;
+      //   console.log('pipe 3');
+      //   const output = await replicate.run(
+      //     "lalalune/segment-anything-direct:60b5ca9c7a29b61c3ddf5f9724efbb9756ff5c5bab094948dd1e905249b7b0d5",
+      //     {
+      //       input: {
+      //         image: dataUri,
+      //       },
+      //     },
+      //   );
+      //   console.log('responding with output 1');
+      //   console.log('responding with output 2', output);
+      //   res.status(200).send(output);
+      } else if (match = req.url.match(/^\/api\/blockadelabs\/(images|depths)/)) {
+        let u = req.url.replace(/^\/api\/blockadelabs/, '');
+        u = `https://blockade-platform-production.s3.amazonaws.com` + u;
+        u = new URL(u);
+        // u.searchParams.set('api_key', this.blockadelabsApiKey);
+        u = u.href;
+
+        await _proxy(req, res, u);
+      } else if (match = req.url.match(/^\/api\/blockadelabs/)) {
+        let u = req.url.replace(/^(\/api)\/blockadelabs/, '$1');
+        u = `https://backend.blockadelabs.com` + u;
+        u = new URL(u);
+        u.searchParams.set('api_key', this.blockadelabsApiKey);
+        u = u.href;
+
+        await _proxy(req, res, u);
       } else if (match = req.url.match(/^\/api\/ai\/(claude\/complete)/)) {
         const proxyRes = await fetch('https://api.anthrophic.com/v1/complete' , {
           method: req.method,
@@ -297,9 +311,6 @@ export class AiServer {
           console.warn('got api error response', text);
           res.status(500).send(text);
         }
-
-
-
       } else {
         console.warn('image client had no url match', req.url);
         res.send(404);
